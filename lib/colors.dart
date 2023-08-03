8a55e805efd9cba5c99abfb20e615f77dbd62f95
yoondabin <yundabin0608@gmail.com>
2023-08-03 16:29:05 -0400

    Feat : add Color

---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color mainGreen1 = Color(0xff96E894);
+const Color mainGreen2 = Color(0xffAFECAE);
+const Color mainOrange = Color(0xffF89F4D);
+
+const Color black = Color(0xff2D2D2D);
+const Color gray = Color(0xff7E7E7D);
+const Color whiteGray = Color(0xffECECE8);
